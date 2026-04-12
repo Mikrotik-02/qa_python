4bf37f3fa80557976e3bb4d912fc71e56ab36e9b
Artem Koval <kovalaqa@mail.ru>
2026-04-12 12:45:16 +0300

    add test for add_new_book duplicate case

--- test_books_collector.py
+++ test_books_collector.py
@@ -18,7 +18,13 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_add_same_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert len(collector.get_books_genre()) == 1
+        assert 'Гордость и предубеждение и зомби' in collector.get_books_genre()
